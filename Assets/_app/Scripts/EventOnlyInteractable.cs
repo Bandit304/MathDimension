8be0246bf59cd3100fabc9434099f91836308b92
Dominic <dominichenryscott@gmail.com>
2024-06-21 18:59:56 -0400

    Added InteractableEvents system
    Can be used to quickly add simple events when an item is interacted with

---
+++ Assets/_app/Scripts/EventOnlyInteractable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class EventOnlyInteractable : Interactable
+{
+    // This is for quickly setting up interactable objects that only need to use the simple interaction event.
+}
--- Assets/_app/Scripts/Interactable.cs
+++ Assets/_app/Scripts/Interactable.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    //Add or remove an InteractionEvent component to this gameObject.
+    public bool useEvents;
     public string promptMessage;
 
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
         Interact();
     }
     protected virtual void Interact()
