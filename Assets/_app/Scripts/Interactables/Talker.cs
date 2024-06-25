1cc4040fd1f457935e51e3cc69e2e399fc356988
Danny Murray <murraydp@mail.uc.edu>
2024-06-24 20:11:02 -0400

    Set talker interactable to disable movement while dialogue is displaying

--- Assets/_app/Scripts/Interactables/Talker.cs
+++ Assets/_app/Scripts/Interactables/Talker.cs
@@ -8,28 +8,27 @@ namespace _app.Scripts.Interactables {
         public string speaker;
         public string text;
 
-        [Header("Interaction Flags")]
-        private bool isInteracting = false;
-
         protected override void Interact() {
-            // If not interacting, display dialogue
-            if (!isInteracting)
-                StartCoroutine(DisplayDialogue());
+            StartCoroutine(DisplayDialogue());
         }
 
         private IEnumerator DisplayDialogue() {
-            // Set isInteracting to true
-            isInteracting = true;
+            // Disable player
+            if (!!InputManager.Instance)
+                InputManager.Instance.DisablePlayer();
             // Display dialogue box
-            DialogueBoxManager.Instance.Display(speaker, text);
+            if (!!DialogueBoxManager.Instance)
+                DialogueBoxManager.Instance.Display(speaker, text);
             // Wait for current interaction to end
             yield return null;
             // Wait until next interaction
             yield return new WaitUntil(() => InputManager.Instance.GetInteracting());
             // Close dialogue box
-            DialogueBoxManager.Instance.Close();
-            // Set isInteracting to false
-            isInteracting = false;
+            if (!!DialogueBoxManager.Instance)
+                DialogueBoxManager.Instance.Close();
+            // Enable player
+            if (!!InputManager.Instance)
+                InputManager.Instance.EnablePlayer();
         }
     }
 }
