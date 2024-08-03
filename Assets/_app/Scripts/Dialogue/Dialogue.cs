dd40570fe517727dbded7d288ef2ba4c9556d746
Danny Murray <murraydp@mail.uc.edu>
2024-08-03 14:51:01 -0400

    Added ability to play dialogue sound effects

--- Assets/_app/Scripts/Dialogue/Dialogue.cs
+++ Assets/_app/Scripts/Dialogue/Dialogue.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using _app.Scripts.Managers;
 using System;
+using Random = UnityEngine.Random;
 
 namespace _app.Scripts.Dialogue {
     [Serializable]
@@ -8,11 +9,16 @@ namespace _app.Scripts.Dialogue {
         public string speaker;
         [TextArea]
         public string text;
+        public AudioClip[] audioClips;
 
         public void Open() {
             // Display dialogue box
             if (!!DialogueBoxManager.Instance)
                 DialogueBoxManager.Instance.Display(speaker, text);
+            if (!!AudioManager.Instance && audioClips?.Length != 0) {
+                AudioClip randomClip = audioClips[Random.Range(0, audioClips.Length)];
+                AudioManager.Instance.PlayGlobalAudio(randomClip);
+            }
         }
 
         public void Close() {
