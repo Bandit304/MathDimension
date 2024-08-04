ce2a4b010a68b8197f289be0c3b2db81e1984b97
Danny Murray <murraydp@mail.uc.edu>
2024-08-04 16:05:35 -0400

    Added environment tint manager

---
+++ Assets/_app/Scripts/Managers/EnvironmentTintManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace _app.Scripts.Managers {
+    public class EnvironmentTintManager : MonoBehaviour {
+        // ===== Fields =====
+
+        public enum Stages { Traditional, Simple, Intermediate, Complex }
+
+        [Header("Singleton Reference")]
+        public static EnvironmentTintManager Instance { get; private set; }
+
+        [Header("Environment Materials")]
+        public Material[] envMaterials;
+
+        [Header("Game Stage")]
+        public Stages stage;
+
+        // ===== Unity Lifecycle Events =====
+
+        // Awake is called once before the first execution of Start after the MonoBehaviour is created
+        void Awake() {
+            if (!Instance)
+                Instance = this;
+            else
+                Destroy(this);
+        }
+
+        public void Start() {
+            switch(stage) {
+                case Stages.Traditional:
+                    AddTint(new Color(135, 211, 255));
+                    break;
+                case Stages.Simple:
+                    AddTint(new Color(207, 255, 215));
+                    break;
+                case Stages.Intermediate:
+                    AddTint(new Color(252, 255, 189));
+                    break;
+                case Stages.Complex:
+                    AddTint(new Color(255, 148, 143));
+                    break;
+            }
+        }
+
+        // ===== Methods =====
+
+        public void AddTint(Color tint) {
+            for (int i = 0; i < envMaterials.Length; i++)
+                envMaterials[i].color = tint;
+        }
+
+    }
+}
