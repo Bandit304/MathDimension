f1c7c544bd42c53888ce423e27f0867ddbfb2b16
Danny Murray <murraydp@mail.uc.edu>
2024-08-05 18:32:11 -0400

    Fixed bug with initializing operator data

--- Assets/_app/Scripts/Operators/OperatorReset.cs
+++ Assets/_app/Scripts/Operators/OperatorReset.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 
 namespace _app.Scripts.Operators
 {
+    // Only attach to scenes that need to initialize the operator array on load
     public class OperatorReset : MonoBehaviour
     {
-        // Only attach to scenes that need to initialize the operator array on load
-        void Start() 
-        {
-            OperatorManager.Instance.InitializeArr();
+        public static OperatorReset Instance { get; private set; }
+
+        // Awake is called once before the first execution of Start after the MonoBehaviour is created
+        void Awake() {
+            if (!Instance)
+                Instance = this;
+            else
+                Destroy(this);
+        }
+
+        public void ResetOperator() {
+            if (!!OperatorManager.Instance)
+                OperatorManager.Instance.InitializeArr();
         }
     }
 }
